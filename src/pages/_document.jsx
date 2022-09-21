98ebf816b82a305cdde17b914fcc2cf6ee64fee7
Tommy Légaré <tommy.legare@uqat.ca>
2022-09-20 22:26:13 -0400

    Tailwind configurations
    Tailwind configurations like headlessui and heroicons.

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -50,6 +50,7 @@ export default function Document() {
           type="application/feed+json"
           href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.json`}
         />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
       <body className="flex h-full flex-col bg-zinc-50 dark:bg-black">
         <Main />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   darkMode: 'class',
   plugins: [require('@tailwindcss/typography')],
   theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
     fontSize: {
       xs: ['0.8125rem', { lineHeight: '1.5rem' }],
       sm: ['0.875rem', { lineHeight: '1.5rem' }],
