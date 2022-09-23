e200137b3396d8bea311e3fd35cb542c0989f08a
Tommy Légaré <tommy.legare@uqat.ca>
2022-09-23 12:12:32 -0400

    Fix: Trying to fix the build

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,26 @@
-import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
-import rehypePrism from '@mapbox/rehype-prism'
+// import nextMDX from '@next/mdx'
+// import remarkGfm from 'remark-gfm'
+// import rehypePrism from '@mapbox/rehype-prism'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['jsx', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    newNextLinkBehavior: true,
-    scrollRestoration: true,
-  },
+  // experimental: {
+  //   newNextLinkBehavior: false,
+  //   scrollRestoration: false,
+  // },
 }
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
-})
+export default nextConfig
 
-export default withMDX(nextConfig)
+// const withMDX = nextMDX({
+//   extension: /\.mdx?$/,
+//   options: {
+//     remarkPlugins: [remarkGfm],
+//     rehypePlugins: [rehypePrism],
+//   },
+// })
+
+// export default withMDX(nextConfig)
