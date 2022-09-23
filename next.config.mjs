26c7a6dd4c29abad9d7ab5ecc41d3a4e038c7736
Tommy Légaré <tommy.legare@uqat.ca>
2022-09-23 14:45:14 -0400

    Setting experimental features (scrollRestoration) to false (turning off)

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,12 @@
-import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
-import rehypePrism from '@mapbox/rehype-prism'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['jsx', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    newNextLinkBehavior: true,
-    scrollRestoration: true,
+    newNextLinkBehavior: false,
+    scrollRestoration: false,
   },
 }
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
-})
-
-export default withMDX(nextConfig)
+export default nextConfig
