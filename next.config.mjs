dcd923bd7d4de55beaba2f33c6a08d5a63d2fff0
Tommy Légaré <tommy.legare@uqat.ca>
2022-09-23 12:18:24 -0400

    Fix: swcMinify to false (comment) to try to fix the build and deploy fail
    Fix: swcMinify to false (comment) to try to fix the build and deploy fail

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@
 const nextConfig = {
   pageExtensions: ['jsx', 'mdx'],
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   // experimental: {
   //   newNextLinkBehavior: false,
   //   scrollRestoration: false,
