e2f26542d7aeb2e41cd0059b3cf6decee0b5faad
Tommy Légaré <tommy.legare@uqat.ca>
2022-09-23 15:20:28 -0400

    Trying to fix the fragment problem by adding a div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -87,50 +87,58 @@ function MobileNavigation(props) {
         <ChevronDownIcon className="ml-3 h-auto w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400" />
       </Popover.Button>
       <Transition.Root>
-        <Transition.Child
-          as={Fragment}
-          enter="duration-150 ease-out"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="duration-150 ease-in"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <Popover.Overlay className="fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur-sm dark:bg-black/80" />
-        </Transition.Child>
-        <Transition.Child
-          as={Fragment}
-          enter="duration-150 ease-out"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="duration-150 ease-in"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <Popover.Panel
-            focus
-            className="fixed inset-x-4 top-8 z-50 origin-top rounded-3xl bg-white p-8 ring-1 ring-zinc-900/5 dark:bg-zinc-900 dark:ring-zinc-800"
+        <div>
+          <Transition.Child
+            as={Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="duration-150 ease-in"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <div className="flex flex-row-reverse items-center justify-between">
-              <Popover.Button aria-label="Close menu" className="-m-1 p-1">
-                <CloseIcon className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
-              </Popover.Button>
-              <h2 className="text-sm font-medium text-zinc-600 dark:text-zinc-400">
-                Navigation
-              </h2>
-            </div>
-            <nav className="mt-6">
-              <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                <MobileNavItem href="/profil">Profil</MobileNavItem>
-                <MobileNavItem href="/curriculumvitae">CV</MobileNavItem>
-                <MobileNavItem href="/enseignement">Enseignement</MobileNavItem>
-                <MobileNavItem href="/recherches">Recherches</MobileNavItem>
-                <MobileNavItem href="/publications">Publications</MobileNavItem>
-                <MobileNavItem href="/contact">Contact</MobileNavItem>
-              </ul>
-            </nav>
-          </Popover.Panel>
-        </Transition.Child>
+            <Popover.Overlay className="fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur-sm dark:bg-black/80" />
+          </Transition.Child>
+        </div>
+        <div>
+          <Transition.Child
+            as={Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-150 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Popover.Panel
+              focus
+              className="fixed inset-x-4 top-8 z-50 origin-top rounded-3xl bg-white p-8 ring-1 ring-zinc-900/5 dark:bg-zinc-900 dark:ring-zinc-800"
+            >
+              <div className="flex flex-row-reverse items-center justify-between">
+                <Popover.Button aria-label="Close menu" className="-m-1 p-1">
+                  <CloseIcon className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
+                </Popover.Button>
+                <h2 className="text-sm font-medium text-zinc-600 dark:text-zinc-400">
+                  Navigation
+                </h2>
+              </div>
+              <nav className="mt-6">
+                <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+                  <MobileNavItem href="/profil">Profil</MobileNavItem>
+                  <MobileNavItem href="/curriculumvitae">CV</MobileNavItem>
+                  <MobileNavItem href="/enseignement">
+                    Enseignement
+                  </MobileNavItem>
+                  <MobileNavItem href="/recherches">Recherches</MobileNavItem>
+                  <MobileNavItem href="/publications">
+                    Publications
+                  </MobileNavItem>
+                  <MobileNavItem href="/contact">Contact</MobileNavItem>
+                </ul>
+              </nav>
+            </Popover.Panel>
+          </Transition.Child>
+        </div>
       </Transition.Root>
     </Popover>
   )
