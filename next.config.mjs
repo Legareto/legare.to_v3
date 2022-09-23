c7859c5edcd0779200d7bb7d742a39b7e634940b
Tommy Légaré <tommy.legare@uqat.ca>
2022-09-23 16:08:56 -0400

    Experimental to true

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
   experimental: {
-    newNextLinkBehavior: false,
+    newNextLinkBehavior: true,
     scrollRestoration: false,
   },
 }
