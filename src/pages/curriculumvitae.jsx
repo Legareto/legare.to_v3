abe4787f94cafb12fcef3fea4a9f3f1cad7ca1a9
Tommy Légaré <tommy.legare@uqat.ca>
2022-09-26 16:08:45 -0400

    cleaning pages/curriculumvitae, some function where not used.

--- src/pages/curriculumvitae.jsx
+++ src/pages/curriculumvitae.jsx
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 
-import { Card } from '@/components/Card'
 import { Button } from '@/components/Button'
-import { Section } from '@/components/Section'
 import { SimpleLayout } from '@/components/SimpleLayout'
 import Resume from '@/components/Resume'
 import Formation from '@/components/Formation'
@@ -12,27 +10,6 @@ import Affiliation from '@/components/Affiliation'
 import Prix from '@/components/Prix'
 import Activite from '@/components/Activite'
 import Technologie from '@/components/Technologie'
-
-function CurriculumvitaeSection({ children, ...props }) {
-  return (
-    <Section {...props}>
-      <div className="space-y-16">{children}</div>
-    </Section>
-  )
-}
-
-function Appearance({ title, description, event, cta, href }) {
-  return (
-    <Card as="article">
-      <Card.Title as="h3" href={href}>
-        {title}
-      </Card.Title>
-      <Card.Eyebrow decorate>{event}</Card.Eyebrow>
-      <Card.Description>{description}</Card.Description>
-      <Card.Cta>{cta}</Card.Cta>
-    </Card>
-  )
-}
 
 export default function Curriculumvitae() {
   return (
