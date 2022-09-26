69cb91e69486bc0b9f757ea6d9f00b89e457d47f
Tommy Légaré <tommy.legare@uqat.ca>
2022-09-26 11:29:56 -0400

    Trying to add a React Fragment

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -64,106 +64,111 @@ export default function Home() {
         />
       </Head>
       <Container className="mt-9">
-        <div className="max-w-2xl">
-          <h1 className="text-4xl font-bold tracking-tight text-teal-500 dark:text-teal-400 sm:text-5xl">
-            Chargé de projet.
-            <br />
-            Chargé de cours.
-            <br />
-            Consultant.
-          </h1>
-          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            Tommy Légaré est chargé de projet en technologies éducatives pour le
-            service de pédagogique universitaire et de formation à distance
-            (SPUFAD) et également chargé de cours à l&apos;unité
-            d&apos;enseignement et de recherche (UER) des sciences de
-            l&apos;éducation de l&apos;UQAT où il enseigne l&apos;intégration
-            des TIC, la didactique de l&apos;univers social et l&apos;histoire
-            du Québec et du Canada.
-          </p>
-          <div className="mt-6 flex gap-6 ">
-            <SocialLink
-              href="https://twitter.com/legaretommy"
-              aria-label="Me suivre sur Twitter"
-              icon={TwitterIcon}
-            />
-            <SocialLink
-              href="https://www.linkedin.com/in/tommylegare/"
-              aria-label="Me suivre sur LinkedIn"
-              icon={LinkedInIcon}
-            />
-            {/* <SocialLink
+        <>
+          <div className="max-w-2xl">
+            <h1 className="text-4xl font-bold tracking-tight text-teal-500 dark:text-teal-400 sm:text-5xl">
+              Chargé de projet.
+              <br />
+              Chargé de cours.
+              <br />
+              Consultant.
+            </h1>
+            <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+              Tommy Légaré est chargé de projet en technologies éducatives pour
+              le service de pédagogique universitaire et de formation à distance
+              (SPUFAD) et également chargé de cours à l&apos;unité
+              d&apos;enseignement et de recherche (UER) des sciences de
+              l&apos;éducation de l&apos;UQAT où il enseigne l&apos;intégration
+              des TIC, la didactique de l&apos;univers social et l&apos;histoire
+              du Québec et du Canada.
+            </p>
+            <div className="mt-6 flex gap-6 ">
+              <SocialLink
+                href="https://twitter.com/legaretommy"
+                aria-label="Me suivre sur Twitter"
+                icon={TwitterIcon}
+              />
+              <SocialLink
+                href="https://www.linkedin.com/in/tommylegare/"
+                aria-label="Me suivre sur LinkedIn"
+                icon={LinkedInIcon}
+              />
+              {/* <SocialLink
               href="https://instagram.com"
               aria-label="Me suivre sur Research Gate"
               icon={InstagramIcon}
             /> */}
-            <SocialLink
-              href="https://github.com/Legareto"
-              aria-label="Me suivre sur GitHub"
-              icon={GitHubIcon}
-            />
-          </div>
-        </div>
+              <SocialLink
+                href="https://github.com/Legareto"
+                aria-label="Me suivre sur GitHub"
+                icon={GitHubIcon}
+              />
+            </div>
+          </div>{' '}
+        </>
       </Container>
+
       <Photos />
-      <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid">
-          {/* max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2 */}
-          {/* <div className="flex flex-col gap-16">
+      <>
+        <Container className="mt-24 md:mt-28">
+          <div className="mx-auto grid">
+            {/* max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2 */}
+            {/* <div className="flex flex-col gap-16">
             {articles.map((article) => (
               <Article key={article.slug} article={article} />
             ))}
           </div> */}
-          <div className="space-y-10 lg:pl-16 xl:pl-24">
-            {/* <Newsletter />
+            <div className="space-y-10 lg:pl-16 xl:pl-24">
+              {/* <Newsletter />
             <Resume /> */}
-          </div>
-          <div className="text-center">
-            <Button
-              href="/profil"
-              variant="primary"
-              className="mr-15 group mt-6"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-6 w-6"
+            </div>
+            <div className="text-center">
+              <Button
+                href="/profil"
+                variant="primary"
+                className="mr-15 group mt-6"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-              Profil
-            </Button>
-            <Button
-              href="/curriculumvitae"
-              variant="primary"
-              className="group mt-6 ml-12"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-6 w-6"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+                Profil
+              </Button>
+              <Button
+                href="/curriculumvitae"
+                variant="primary"
+                className="group mt-6 ml-12"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
-                />
-              </svg>
-              Curriculum vitae
-            </Button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
+                  />
+                </svg>
+                Curriculum vitae
+              </Button>
+            </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </>
     </>
   )
 }
